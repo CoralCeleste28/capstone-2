500d56c80c85148a033bf64e7830f9177c0cba9d
Coral Estrada <coralestrada28@gmail.com>
2025-11-08 16:45:11 -0600

    Made classes and added visible items in UserInterface class.

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,34 @@
+public class UserInterface {
+
+    public static void main(String[] args) {
+    }
+
+    public void homeScreen (){
+        System.out.printf("""
+                Welcome to Chiqui's Tacos Near Me!
+                Press 1 to place an order
+                Press 2 to exit
+                """);
+    }
+
+    public void orderScreen(){
+        System.out.printf("""
+                Please choose from the following:
+                1 - Add Taco
+                2 - Add Drink
+                3 - Add Chips and Salsa
+                4 - Check Out
+                0 - Cancel Order
+                """);
+    }
+
+    public void checkOut(){
+        System.out.printf("""
+                Please choose from the following:
+                1 - Confirm Order
+                0 - Cancel Order
+                """);
+    }
+
+
+}
